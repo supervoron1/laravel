1eb0e28bd6eebc849d47cf455636f8a00c5896de
Dmitriy Vorontsov <vorontsof@mail.ru>
2020-08-08 17:21:31 +0400

    базовые шаблоны и роуты

---
+++ resources/views/menu.php
@@ -0,0 +1,3 @@
+<a href="/">Главная</a>
+<a href="/about">О проекте</a>
+<a href="/news">Новости</a><br>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/news', function () {
+    return view('news');
 });
